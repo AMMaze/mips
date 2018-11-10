9ed8090307df912cdaaab661f5460cf57f0ab945
Marat Abbas Mazen <unlock96@gmail.com>
2018-11-10 20:54:38 +0300

    removed unused module

--- modules/adder.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns/1ns
-module adder
-  #(parameter Width = 32)
-  ( input [Width-1:0] in1,
-    input [Width-1:0] in2,
-    output [Width-1:0] out
-  );
-  assign out = in1 + in2;
-endmodule
